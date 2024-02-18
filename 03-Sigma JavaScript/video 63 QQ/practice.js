aca9c10667f563603c213df20e05193bdfdd34a6
Dr-MAhmed <dr.muhammadahmed05@gmail.com>
2024-02-18 13:35:01 +0500

    sigma javascript

--- 03-Sigma JavaScript/video 63 QQ/practice.js
+++ 03-Sigma JavaScript/video 63 QQ/practice.js
@@ -1,14 +1,14 @@
 // Practice set Chapter 05
 
 //Problem No 01
-// Create an array of numbers and take input from user to add number to this array.
+//Create an array of numbers and take input from user to add number to this array.
 let arr = [1,2,3];
 let a = prompt('Enter a number');
 a = Number.parseInt(a);
 arr.push(a);
 console.log(arr);
 
-// Problem No 02
+//Problem No 02
 //Keep adding number to the array in Q-1 until 0 is added to the array.
 let arr1 = [1,2,3];
 let a1;
@@ -19,7 +19,7 @@ do {
 } while (a1!=0);
 console.log(arr1);
 
-// Problem No 03
+//Problem No 03 
 //Filters for numbers divisible by 10 from a given array.
 let arr2 = [1,2,3,4,20,5,23,55,60,9,70,90,760]
 let veri = arr2.filter((e)=>{
